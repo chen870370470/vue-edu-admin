677feb3bd71087b9484eda7f4f5e4b07d5f3edfd
chenjikang <870370470@qq.com>
2021-04-05 23:46:56 +0800

    基本布局，侧边栏、头部组件完成

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div id="app">
-    <h1>App</h1>
     <!-- 根路由出口 -->
     <router-view />
   </div>
 </template>
+<script lang="ts">
+// 1.编辑器给的类型提示
+// 2.TypeScript编译期间的类型验证
+import Vue from 'vue'
 
+export default Vue.extend({
+  name: 'App'
+})
+</script>
 <style lang="scss" scope>
+
 </style>
