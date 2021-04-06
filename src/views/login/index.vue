a83fd9da2ea2227e1368fecf4083a64644b43331
chenjikang <870370470@qq.com>
2021-04-07 00:18:36 +0800

    登录页面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,107 @@
 <template>
-  <div class="login">Login管理</div>
+  <div class="login">
+    <el-form :model="form" :rules="rules" ref="form" class="login-form" label-width="80px"
+      label-position="top">
+      <h1>登录</h1>
+      <el-form-item label="手机号" prop="phone">
+        <el-input v-model="form.phone"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input type="password" v-model="form.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button class="login-btn" type="primary" @click="onSubmit" :loading="isLoginLoading">登录
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { Form } from 'element-ui'
+import { login } from '@/services/user'
 
 export default Vue.extend({
-  name: 'LoginIndex'
+  name: 'LoginIndex',
+  data () {
+    return {
+      form: {
+        phone: '18201288771',
+        password: '111111'
+      },
+      rules: {
+        phone: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          {
+            pattern: /^1\d{10}$/,
+            message: '请输入正确的手机号',
+            trigger: 'blur'
+          }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          {
+            min: 6,
+            max: 18,
+            message: '长度在 6 到 18 个字符',
+            trigger: 'blur'
+          }
+        ]
+      },
+      isLoginLoading: false
+    }
+  },
+  methods: {
+    async onSubmit () {
+      try {
+        // 1.表单验证
+        await (this.$refs.form as Form).validate()
+        // 登录按钮loading
+        this.isLoginLoading = true
+        // 2.验证通过=> 提交表单
+        const { data } = await login(this.form)
+        // const { data } = await request({
+        //   method: 'POST',
+        //   url: '/front/user/login',
+        //   headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        //   data: qs.stringify(this.form) // axios 默认发送的是application/json格式的数据
+        // })
+        // 3.处理请求结果
+        if (data.state !== 1) {
+          this.$message.error(data.message)
+        } else {
+          this.$router.push({
+            name: 'home'
+          })
+          this.$message.success('登陆成功')
+          //    成功：跳转到首页
+          //    失败：给出提示
+        }
+      } catch (error) {
+        console.log('登陆失败', error)
+      }
+      // 取消登录按钮loading
+      this.isLoginLoading = false
+    }
+  }
 })
 </script>
 
 <style lang="scss" scoped>
-
+.login {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-form {
+    width: 300px;
+    background: #fff;
+    padding: 20px;
+    border-radius: 5px;
+  }
+  .login-btn {
+    width: 100%;
+  }
+}
 </style>
