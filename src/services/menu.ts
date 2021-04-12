ce04656a9b073716ff05595e2601b24bedaab2d5
chenjikang <870370470@qq.com>
2021-04-12 21:10:22 +0800

    角色管理

--- src/services/menu.ts
+++ src/services/menu.ts
@@ -32,3 +32,25 @@ export const deleteMenus = (id: number) => {
     url: `/boss/menu/${id}`
   })
 }
+export const getMenuNodeList = () => {
+  return request({
+    method: 'GET',
+    url: '/boss/menu/getMenuNodeList'
+  })
+}
+export const allocateRoleMenus = (data: any) => {
+  return request({
+    method: 'POST',
+    url: '/boss/menu/allocateRoleMenus',
+    data
+  })
+}
+export const getRoleMenus = (roleId: string | number) => {
+  return request({
+    method: 'GET',
+    url: '/boss/menu/getRoleMenus',
+    params:{ // axios会把params转换为key=value&key=value的数据格式放到url后面，以？分割
+      roleId
+    }
+  })
+}
--- src/views/role/alloc-menu.vue
+++ src/views/role/alloc-menu.vue
@@ -1,15 +1,86 @@
 <template>
-  <div class="alloc-menu">分配菜单</div>
+  <div class="alloc-menu">
+    <el-card :body-style="{ padding: '20px' }">
+      <div slot="header">
+        <span>分配权限</span>
+      </div>
+      <el-tree ref="menu-tree" node-key="id" :data="menus" :props="defaultProps"
+        :default-checked-keys="checkedKeys" show-checkbox default-expand-all></el-tree>
+      <div style="text-align: center">
+        <el-button @click="resetChecked">清空</el-button>
+        <el-button type="primary" @click="onSave">保存</el-button>
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import {
+  getMenuNodeList,
+  allocateRoleMenus,
+  getRoleMenus
+} from '@/services/menu'
+import { Tree } from 'node_modules/element-ui'
+
 export default Vue.extend({
   name: 'AllocMenu',
   props: {
     roleId: {
       type: [String, Number],
       required: true
+    }
+  },
+  data () {
+    return {
+      menus: [],
+      defaultProps: {
+        children: 'subMenuList',
+        label: 'name'
+      },
+      checkedKeys: []
+    }
+  },
+  created () {
+    this.loadMenus()
+    this.loadRoleMenus()
+  },
+  methods: {
+    async loadMenus () {
+      const { data } = await getMenuNodeList()
+      this.menus = data.data
+    },
+    async loadRoleMenus () {
+      const { data } = await getRoleMenus(this.roleId)
+      this.getCheckedKeys(data.data)
+    },
+    // eslint-disable-next-line
+    getCheckedKeys(menus: any) {
+      // eslint-disable-next-line
+      menus.forEach((menu: any) => {
+        if (menu.selected) {
+          // this.checkedKeys.push(menu.id as never)
+          // eslint-disable-next-line
+          this.checkedKeys = [...this.checkedKeys, menu.id] as any
+        }
+        if (menu.subMenuList) {
+          this.getCheckedKeys(menu.subMenuList)
+        }
+      })
+    },
+    resetChecked () {
+      (this.$refs['menu-tree'] as Tree).setCheckedKeys([])
+    },
+    async onSave () {
+      // 拿到选中节点的数据id列表
+      // 请求提交保存
+      const menuIdList = (this.$refs['menu-tree'] as Tree).getCheckedKeys()
+      await allocateRoleMenus({
+        roleId: this.roleId,
+        menuIdList
+      })
+      this.$message.success('操作成功')
+      this.$router.back()
     }
   }
 })
